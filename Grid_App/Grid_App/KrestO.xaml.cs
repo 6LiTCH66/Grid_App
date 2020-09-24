6dffbeef94410d47f72c1eb45726ea9b0c8f36fc
opilane <opilane@HP-CZC6297B8F>
2020-09-24 10:44:38 +0300

    Trips traps trull

--- Grid_App/Grid_App/KrestO.xaml.cs
+++ Grid_App/Grid_App/KrestO.xaml.cs
@@ -12,10 +12,14 @@ namespace Grid_App
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class KrestO : ContentPage
     {
-        Button btn, btn1;
+        Button btn, btn1, btn2;
         BoxView box;
-        public KrestO()
+        Label lbl;
+        Image img;
+        Grid grid;
+        void NewGame()
         {
+
             Grid grid = new Grid();
             for (int i = 0; i < 3; i++)
             {
@@ -26,33 +30,68 @@ namespace Grid_App
             {
                 for (int j = 0; j < 3; j++)
                 {
-                    box = new BoxView
-                    {
-                        Color = Color.FromRgb(200, 100, 50)
-                    };
+                    img = new Image { Source = ImageSource.FromFile("nn.png") };
 
-                    grid.Children.Add(box, i, j);
+                    grid.Children.Add(img, i, j);
                     var tap = new TapGestureRecognizer();
                     tap.Tapped += Tap_Tapped;
-                    box.GestureRecognizers.Add(tap);
+                    img.GestureRecognizers.Add(tap);
                 }
             }
+
+
             btn = new Button()
             {
                 Text = "Reset the Game!",
             };
             btn.Clicked += Btn_Clicked;
 
+
             btn1 = new Button()
             {
-                Text = "Test",
+                FontSize = 24,
+                Text = "X / 0",
             };
+            btn1.Clicked += Btn1_Clicked;
+
+
+            lbl = new Label()
+            {
+                Text = "",
+                FontSize = 34,
+                VerticalOptions = LayoutOptions.Center,
+                HorizontalOptions = LayoutOptions.Center,
+            };
+
 
             StackLayout stackLayout = new StackLayout()
             {
-                Children = {grid, btn, btn1}
+                BackgroundColor = Color.Blue,
+                Children = { grid, btn, btn1, lbl }
             };
             Content = stackLayout;
+        }
+        public KrestO()
+        {
+            btn2 = new Button()
+            {
+                Text = "Start",
+            };
+
+            btn2.Clicked += Btn2_Clicked;
+
+            StackLayout stackLayout = new StackLayout()
+            {
+                Children = { btn2 }
+            };
+            Content = stackLayout;
+
+        }
+
+        private void Btn2_Clicked(object sender, EventArgs e)
+        {
+            btn2.IsVisible = false;
+            NewGame();
         }
 
         
@@ -60,23 +99,52 @@ namespace Grid_App
         int tp = 0;
         private void Tap_Tapped(object sender, EventArgs e)
         {
-            BoxView box = sender as BoxView;
+            Image img1 = sender as Image;
 
             tp++;
-            
-            if (tp == 1)
+            if (tp == 2)
             {
-                box.Color = Color.Black;
+                img1.Source = ImageSource.FromFile("X.png");
+                if (img1.Source  == img1.Source)
+                {
+                    img1.IsEnabled = false;
+                }
             }
             else if(tp == 3)
             {
-                box.Color = Color.White;
+                img1.Source = ImageSource.FromFile("O.png");
+                if (img1.Source == img1.Source)
+                {
+                    img1.IsEnabled = false;
+                }
                 tp = 0;
             }
         }
-        private void Btn_Clicked(object sender, EventArgs e)
+        private void Btn_Clicked(object sender, EventArgs e)  // button Reset the Game
         {
-            
+            NewGame();
+            btn1.IsEnabled = true;
+        }
+
+        Random rnd = new Random();
+
+        private void Btn1_Clicked(object sender, EventArgs e)
+        {
+
+            int rand = rnd.Next(1, 3);
+
+            if (rand == 1)
+            {
+                lbl.Text = "X";
+                tp = 1;
+                
+            }
+            else if (rand == 2)
+            {
+                lbl.Text = "0";
+                tp = 2;
+            }
+            btn1.IsEnabled = false;
         }
     }
 }
